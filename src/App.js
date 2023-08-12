103c961b0d8b87e4d40b79836834adc496afe773
Shubam Bharti <shubambharti1998@gmail.com>
2023-08-13 01:53:02 +0530

    Avatar created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Profile from './components/profile/Profile.tsx'
+import Avatar from './components/avatar/index.tsx'
 
 function App() {
   return (
     <div className="App">
         <Profile/>
+        <Avatar/>
     </div>
   );
 }
--- src/components/avatar/index.tsx
+++ src/components/avatar/index.tsx
@@ -1,7 +1,11 @@
-export function Avatar(){
-    return(
-        <div>
-            Avatar
-        </div>
-    )
-}
+ import * as React from 'react';
+ import "./style.css";
+ 
+ export default function Avatar() {
+   return (
+    <div >
+      <img className="image" src = {require("/Users/shubam_maverick/Documents/tcube/TcubeFrontend/src/components/avatar/images/default.png")} />
+    </div>
+    
+   );
+ }
